142ef64878eb80b4659ab10dd6482f3b9bf11782
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-06-03 02:54:47 -0700

    start the session

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+import cookieParser from "cookie-parser"
 import express from "express"
 
 const app = express()
+app.use(cookieParser())
 
 app.get("/",(req,res)=>{
+    res.cookie("name","muhammad")
+    res.cookie("ali","lhan")
     res.send("hello")
 })
+
+app.get("/ali",(req,res)=>{
+   console.log(req.cookies)
+    res.send("hello.hellio")
+})
+
 app.listen(3000,()=>{
     console.log("http://localhost:3000");
     
